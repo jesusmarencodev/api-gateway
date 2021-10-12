7c047c24776d7f8f32ea552ac013df975711a033
Jesus Marenco <jesusmarencodev@gmail.com>
2021-10-12 07:53:01 -0500

    controllers ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { PassengerModule } from './passenger/passenger.module';
+import { FlightModule } from './flight/flight.module';
 
 @Module({
   imports: [
@@ -10,7 +12,9 @@ import { UserModule } from './user/user.module';
       envFilePath:['.env.development'],
       isGlobal : true
     }),
-    UserModule
+    UserModule,
+    PassengerModule,
+    FlightModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exceptiopn.filter';
 import { TimeOutInterceptor } from './common/interceptors/timeOut.interceptor';
@@ -7,6 +8,23 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new TimeOutInterceptor());
+
+  const options = new DocumentBuilder()
+    .setTitle('Flight API')
+    .setDescription('Scheduled Flight App')
+    .setVersion('2.0.0')
+    //.addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('/api/docs', app, document, {
+    swaggerOptions : {
+      filter :true,
+    }
+  })
+
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
